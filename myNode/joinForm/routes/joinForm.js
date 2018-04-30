0361be8210ec2a95bac09b9c7343f66d94ec8009
RosieInThePark <325rabbit@gmail.com>
2018-04-30 19:51:52 +0900

    update joinForm.js to print User's input to JSON

--- myNode/joinForm/routes/joinForm.js
+++ myNode/joinForm/routes/joinForm.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('joinForm', {title : '2014722030 HyunahPark : Join Form!'});
 });
 
+/*print User's input to JSON*/
+router.post('/', function(req, res, next){
+  console.log(req.body);
+  res.json(req.body);
+});
 module.exports = router;
