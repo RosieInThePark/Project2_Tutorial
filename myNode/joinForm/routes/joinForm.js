027aa332ad129bd5222d5d318e6d6a8bf944bfea
RosieInThePark <325rabbit@gmail.com>
2018-04-30 19:39:33 +0900

    create joinForm.js and print "2014722030 Hyunah Park joinForm.js check"

---
+++ myNode/joinForm/routes/joinForm.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('2014722030 Hyunah Park joinForm.js check');
+});
+
+module.exports = router;
