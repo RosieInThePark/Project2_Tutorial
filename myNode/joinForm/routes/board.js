cb419f09f6ae9ce0b9bdd9c8d7cc0d4956f1d58d
RosieInThePark <325rabbit@gmail.com>
2018-05-04 10:28:28 +0900

    글쓰기 로직 처리 POST, board.js 수정

--- myNode/joinForm/routes/board.js
+++ myNode/joinForm/routes/board.js
@@ -32,9 +32,36 @@ router.get('/list/:page', function(req, res, next) {
     });
   });
 });
+
 //글쓰기 화면 표시 GET
 router.get('/write', function(req, res, next) {
   res.render('write', {title : "2014722030 Hyunah Park 게시판 글 쓰기"});
 });
+
+//글쓰기 로직 처리 POST
+router.post('/write', function(req,res,next){
+  var creator_id = req.body.creator_id;
+  var title = req.body.title;
+  var content = req.body.content;
+  var passwd = req.body.passwd;
+  var datas = [creator_id, title, content, passwd];
+
+  pool.getConnection(function(err, connection) {
+    //Use the connection
+    var sqlForInsertBoard = "insert into board(creator_id, title, content, passwd) values(?,?,?,?)";
+    connection.query(sqlForInsertBoard, datas, function(err, rows){
+      if(err) console.error("err : " + err);
+      console.log("rows : " + JSON.stringify(rows));
+
+      res.redirect('/board');
+      connection.release();
+
+      //Don't use the connection here, it has been returned to the pool.
+    });
+  });
+});
+
+
+
 
 module.exports = router;
